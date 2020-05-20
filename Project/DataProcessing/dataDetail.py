1720889d93682630a7586995a6e19915361ab318
mtb-hust <manhtruong6122@gmail.com>
2020-05-20 10:03:28 +0700

    script

---
+++ Project/DataProcessing/dataDetail.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import csv 
+import numpy as np
+import os
+
+cwd = os.getcwd() #get current working directory
+print(cwd)
+
+splittedDataFolder = cwd + "/Data/splittedDataWithLabels/" #direct to containing folder
+files = os.listdir(splittedDataFolder)
+
+with open("./Data/analyze.csv", "+a") as file_x:
+    writer = csv.writer(file_x, delimiter =  ",")
+    writer.writerow(["ProtocolName","MinSourcePort", "MaxSourcePort", "MinDesport", "MaxDesport","MinFlowDur","MaxFlowDur","MinFlowBytes"
+        , "MaxFlowBytes", "MinFlowPackets", "MawFlowPackets", "MinAvgPacketSize", "MaxAvgPacketSize"
+        ])
+    for file in files:
+        df = pd.read_csv("./Data/labelledData/" + file)
+        data = [file]
+        for col in df.columns:
+            if(col == "SourceIP" or col == "DesIP" or  col ==  "ProtocolName"):
+                continue
+            temp = df[col].to_numpy()
+            min1 = temp.min()
+            max2 = temp.max()
+            print(f"{col} + {min1} +{max2}")
+            data.append(min1)
+            data.append(max2)
+        writer.writerow(data)
+        print(data)
+    
+
+            
+        
