154f8a1a65b022c4c6a9a9b80929b38e1d6d2f75
mtb-hust <manhtruong6122@gmail.com>
2020-06-03 11:40:17 +0700

    higher accuracy model

--- Project/DataProcessing/test_svm.py
+++ Project/DataProcessing/test_svm.py
@@ -3,7 +3,7 @@ from sklearn.svm import LinearSVC
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
-clf = LinearSVC()
+clf = LinearSVC( C = 20)
 data,label = ReadData.read_data()
 shape = label.shape 
 one_hot_label = np.zeros((shape[0], 20))
@@ -12,11 +12,9 @@ testLabel = []
 for i in range(shape[0]):
     one_hot_label[i][label[i]] = 1
 for i in range(shape[0]):
-    if i %5 == 0:
+    if i%5 == 0:
         testData.append(data[i])
         testLabel.append(label[i])
-    if i > 2026980:
-        break
 X_test, X_train, Y_test, Y_train = train_test_split(testData, testLabel, test_size = 0.30, shuffle = True)
 clf.fit(X_train, Y_train)
 print(clf.score(X_test,Y_test))
